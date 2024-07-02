49d5e80a52ef0e7922fb53b1cf461f590f6075b5
Akshat16-tech <akshatgandhi77@gmail.com>
2024-07-01 20:39:19 -0400

    Removed code from signup route.ts

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -5,36 +5,7 @@ import connectDB from '@/config/connectDB';
 import { cookies } from 'next/headers'
 connectDB();
 export async function POST(request: NextRequest, response: NextResponse){
-  // try {
-  //   const cookieStore = cookies();
-  //   const userObject = cookieStore.get('user');
-  //   const userEncoded = userObject.value;
-  //   const email = Buffer.from(userEncoded, 'base64').toString('utf-8');
 
-  //   const userDetails = await User.findOne({ email }).lean();
-
-  //   if (userDetails) {
-  //     console.log('User already exists');
-  //     return NextResponse.json(
-  //       { error: 'User already exists' },
-  //       { status: 400 }
-  //     );
-  //   }
-  //   console.log(userDetails);
-  //   const newUser = await User.create({
-  //     email: email,
-  //   });
-
-  //   const response = NextResponse.json({
-  //     message: 'Register successful',
-  //     success: true,
-  //     user: newUser,
-  //   });
-
-  //   return response;
-  // } catch (error: any) {
-  //   return NextResponse.json({ error: error.message }, { status: 500 });
-  // }
   try {
     const { email } = await request.json();
     let user = await User.findOne({ email });
